11d40055f0524e42223eeae1d6681266c2c0be56
AdirGamil <2000adir2000@gmail.com>
2024-06-26 20:18:32 +0300

    Add input css

--- apps/note/cmps/NoteAdd.jsx
+++ apps/note/cmps/NoteAdd.jsx
@@ -1,15 +1,29 @@
 const { useEffect, useState } = React
 
-
 export function NoteAdd() {
   return (
     <section className="add-note-main">
       <div className="input-with-buttons">
-        <input type="text" placeholder="What's on your mind..." />
-        <button onClick={() => console.log("Button 1 clicked")}>B1</button>
-        <button onClick={() => console.log("Button 2 clicked")}>B2</button>
-        <button onClick={() => console.log("Button 3 clicked")}>B3</button>
+        <input className="note-text" type="text" placeholder="What's on your mind..." />
+        <button
+          className="note-input-btn"
+          onClick={() => console.log('Button 1 clicked')}
+        >
+          <span class="material-symbols-outlined">add_comment</span>
+        </button>
+        <button
+          className="note-input-btn"
+          onClick={() => console.log('Button 2 clicked')}
+        >
+          <span class="material-symbols-outlined">add_photo_alternate</span>
+        </button>
+        <button
+          className="note-input-btn"
+          onClick={() => console.log('Button 3 clicked')}
+        >
+          <span class="material-symbols-outlined">youtube_activity</span>
+        </button>
       </div>
     </section>
-  );
+  )
 }
