a5ebfd75a2570be505cf3c96e3af449193277701
AdirGamil <2000adir2000@gmail.com>
2024-06-27 10:01:41 +0300

    Create notes bgc

--- apps/note/cmps/NotePreview.jsx
+++ apps/note/cmps/NotePreview.jsx
@@ -8,24 +8,22 @@ export function NotePreview({ notes, onRemoveNote }) {
   return (
     <section className="note-list">
       {notes.map((note) => (
-        <div key={note.id} className="note">
+        <div
+          key={note.id}
+          style={{ backgroundColor: note.style.backgroundColor }}
+          className="note"
+        >
           <h1 className="note-title">{note.info.title}</h1>
           <p className="note-txt">{note.info.txt}</p>
           <div className="note-btns">
             <button onClick={() => onRemoveNote(note.id)} className="note-btn">
-            <span className="material-symbols-outlined">
-                        delete
-                    </span>
+              <span className="material-symbols-outlined">delete</span>
             </button>
             <button className="note-btn">
-            <span className="material-symbols-outlined ">
-                        push_pin
-                    </span>
+              <span className="material-symbols-outlined ">push_pin</span>
             </button>
             <button className="note-btn">
-            <span className="material-symbols-outlined">
-                        palette
-                    </span>
+              <span className="material-symbols-outlined">palette</span>
             </button>
           </div>
         </div>
@@ -33,5 +31,3 @@ export function NotePreview({ notes, onRemoveNote }) {
     </section>
   )
 }
-
-
