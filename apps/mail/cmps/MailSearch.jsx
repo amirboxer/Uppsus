7626a1893e19d91ddcfb5bb2a2d3ced607ec9eb9
amirboxer <amirboxer95@gmail.com>
2024-06-29 20:40:19 +0300

    Home css location

--- apps/mail/cmps/MailSearch.jsx
+++ apps/mail/cmps/MailSearch.jsx
@@ -8,7 +8,7 @@ export function MailSearch({ prevPattern, setPrevPattern }) {
 
     return (
         <div className="search-input">
-            <span class="material-icons">search</span>
+            <span className="material-icons">search</span>
             <input type="text" value={prevPattern.subject} placeholder="Search mail" onChange={onChangePattern} />
         </div>
     )
