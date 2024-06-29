4abf5738e9ea8416079949cbbe723e9d86b4b96c
AdirGamil <2000adir2000@gmail.com>
2024-06-29 14:26:37 +0300

    Changes at details

--- pages/About.jsx
+++ pages/About.jsx
@@ -7,7 +7,7 @@ export function About() {
         <div className="card">
             <img className="card-img" src="https://ca.slack-edge.com/T070S9J580H-U070GT12WG4-f7fe57d49b4a-512" alt="" />
             <h1 className="card-title">Adir Gamil</h1>
-            <p className="card-details">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt maiores voluptates rem inventore alias maxime eveniet temporibus sequi mollitia ipsam exercitationem, tempore, iure molestiae. Totam laborum facere sed ducimus voluptatibus!</p>
+            <p className="card-details">Full Stack Developer based in Israel, initiated my journey at "Coding Academy", committed to continuous growth and excellence!</p>
             <div className="card-btns">
                 <a className="card-btn" href="https://github.com/AdirGamil"><i class="fa-brands fa-github"></i></a>
                 <a className="card-btn" href="https://www.linkedin.com/in/adirg/"><i class="fa-brands fa-linkedin-in"></i></a>
@@ -17,7 +17,7 @@ export function About() {
         <div className="card">
             <img className="card-img" src="https://cdn.discordapp.com/attachments/1255503465619263581/1256559148762533908/image.png?ex=66813566&is=667fe3e6&hm=fa15142b20a4561f0d9b9de87d73173dfd21949bc71dd20556f41bb0e5b25c50&" alt="" />
             <h1 className="card-title">Amir Boxer</h1>
-            <p className="card-details">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt maiores voluptates rem inventore alias maxime eveniet temporibus sequi mollitia ipsam exercitationem, tempore, iure molestiae. Totam laborum facere sed ducimus voluptatibus!</p>
+            <p className="card-details">Full Stack Developer, Israel, I honed my skills at "Coding Academy" with a relentless pursuit of mastery and innovation! Holds a BSc in Computer Science.</p>
             <div className="card-btns">
                 <a className="card-btn" href="https://github.com/amirboxer"><i class="fa-brands fa-github"></i></a>
                 <a className="card-btn" href="https://www.linkedin.com/in/amir-boxer/"><i class="fa-brands fa-linkedin-in"></i></a>
