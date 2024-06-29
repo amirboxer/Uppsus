dc4fdb2e5e23d50afee77168b1faf33ac8a73e9a
amirboxer <amirboxer95@gmail.com>
2024-06-29 14:27:20 +0300

    add hover on google

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -16,21 +16,39 @@ export function AppHeader() {
         </Link>
 
         <div className="app-header-right-handside">
+            {/* home Link */}
             <NavLink className="home" title="Home" to="/">Home</NavLink>
+
+            {/* abou link */}
             <NavLink className="about" title="About" to="/about">About</NavLink>
+
+            {/* apps links */}
+            <button className="apps-icon material-icons" title="Apps" onClick={toggleDisplay}>apps</button>
             <nav className={`main-nav ${displayNav ? '' : 'display-none'}`}>
+                {/* mail */}
                 <NavLink to="/mail" onClick={toggleDisplay}>
                     <img src="./assets/img/gmail-icon.png" alt="" />
                     <span>Gmail</span>
                 </NavLink>
+
+                {/* notes */}
                 <NavLink to="/note" onClick={toggleDisplay}>
                     <img src="./assets/img/keep-icon.png" alt="" />
                     <span>Keep</span>
                 </NavLink>
             </nav>
-            <button className="apps-icon material-icons" title="Apps" onClick={toggleDisplay}>apps
+
+            {/* user info */}
+            <button className="user-icon">{mailService.getUserName()[0]}
+                <div className="user-info">
+                    <div className="appsus-account">Appsus Account</div>
+                    <div  className="user-name">{mailService.getUserName()}</div>
+                    <div className="user-mail">{mailService.getUserMail()}</div>
+                </div>
+
             </button>
-            <button className="user-icon">{mailService.getUserName()[0]}</button>
+
+
         </div>
     </header>
 }
