b2d2865983bfd461a155d72835ee3021b15a11df
AdirGamil <2000adir2000@gmail.com>
2024-06-29 14:01:46 +0300

    Add about section

--- pages/About.jsx
+++ pages/About.jsx
@@ -1,6 +1,29 @@
 
 export function About() {
-    return <section className="about">
-        <h1>About Page</h1>
+    return <section className="about-main">
+        <h1 className="about-title">Our Team</h1>
+<section className="cards-container">
+
+        <div className="card">
+            <img className="card-img" src="https://ca.slack-edge.com/T070S9J580H-U070GT12WG4-f7fe57d49b4a-512" alt="" />
+            <h1 className="card-title">Adir Gamil</h1>
+            <p className="card-details">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt maiores voluptates rem inventore alias maxime eveniet temporibus sequi mollitia ipsam exercitationem, tempore, iure molestiae. Totam laborum facere sed ducimus voluptatibus!</p>
+            <div className="card-btns">
+                <a className="card-btn" href="https://github.com/AdirGamil"><i class="fa-brands fa-github"></i></a>
+                <a className="card-btn" href="https://www.linkedin.com/in/adirg/"><i class="fa-brands fa-linkedin-in"></i></a>
+            </div>
+        </div>
+
+        <div className="card">
+            <img className="card-img" src="https://cdn.discordapp.com/attachments/1255503465619263581/1256559148762533908/image.png?ex=66813566&is=667fe3e6&hm=fa15142b20a4561f0d9b9de87d73173dfd21949bc71dd20556f41bb0e5b25c50&" alt="" />
+            <h1 className="card-title">Amir Boxer</h1>
+            <p className="card-details">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt maiores voluptates rem inventore alias maxime eveniet temporibus sequi mollitia ipsam exercitationem, tempore, iure molestiae. Totam laborum facere sed ducimus voluptatibus!</p>
+            <div className="card-btns">
+                <a className="card-btn" href="https://github.com/amirboxer"><i class="fa-brands fa-github"></i></a>
+                <a className="card-btn" href="https://www.linkedin.com/in/amir-boxer/"><i class="fa-brands fa-linkedin-in"></i></a>
+            </div>
+        </div>
+</section>
+
     </section>
 }
