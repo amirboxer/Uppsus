73201d2e3fe6891f2b0a49c5ab361b7e9a728c6c
amirboxer <amirboxer95@gmail.com>
2024-06-29 14:04:38 +0300

    Aplly Css To app header

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -1,28 +1,36 @@
 const { Link, NavLink } = ReactRouterDOM
 import { mailService } from "../apps/mail/services/mail.service.js"
+const { useState } = React
+
 
 export function AppHeader() {
+    const [displayNav, setDisplayNav] = useState(false)
+
+    function toggleDisplay() {
+        setDisplayNav(prevDisplay => !prevDisplay)
+    }
 
     return <header className="app-header">
         <Link to="/">
             <h3>LOGO!</h3>
         </Link>
 
-        <button className="apps-icon material-icons" title="Apps">apps
-            <nav className="main-nav">
-                <NavLink to="/">Home</NavLink>
-                <NavLink to="/about">About</NavLink>
-                <NavLink to="/mail">Mail</NavLink>
-                <NavLink to="/note">Note</NavLink>
+        <div className="app-header-right-handside">
+            <NavLink className="home" title="Home" to="/">Home</NavLink>
+            <NavLink className="about" title="About" to="/about">About</NavLink>
+            <nav className={`main-nav ${displayNav ? '' : 'display-none'}`}>
+                <NavLink to="/mail" onClick={toggleDisplay}>
+                    <img src="./assets/img/gmail-icon.png" alt="" />
+                    <span>Gmail</span>
+                </NavLink>
+                <NavLink to="/note" onClick={toggleDisplay}>
+                    <img src="./assets/img/keep-icon.png" alt="" />
+                    <span>Keep</span>
+                </NavLink>
             </nav>
-        </button>
-        <button className="user-icon">{mailService.getUserName()[0]}</button>
-
-        {/* <nav>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/about">About</NavLink>
-            <NavLink to="/mail">Mail</NavLink>
-            <NavLink to="/note">Note</NavLink>
-        </nav> */}
+            <button className="apps-icon material-icons" title="Apps" onClick={toggleDisplay}>apps
+            </button>
+            <button className="user-icon">{mailService.getUserName()[0]}</button>
+        </div>
     </header>
 }
